085739187659e437edae2f6102b861b966695e01
gpalabgeorgia <gpalabgeorgia@gmail.com>
2024-10-11 19:57:03 +0200

    Added products images table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(SectionsTableSeeder::class);
         // $this->call(CategoryTableSeeder::class);
         // $this->call(ProductsTableSeeder::class);
-        $this->call(ProductsAttributesTableSeeder::class);
+        // $this->call(ProductsAttributesTableSeeder::class);
+        $this->call(ProductsImagesTableSeeder::class);
     }
 }
---
+++ database/seeders/ProductsImagesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ProductsImage;
+
+class ProductsImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $productImageRecords = [
+            ['id'=>1, 'product_id'=>1, 'image'=>'logo.jpeg-83357.jpeg','status'=>1]
+        ];
+        ProductsImage::insert($productImageRecords);
+    }
+}
