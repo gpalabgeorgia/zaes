ebffa4722efe7f1635ce03765c8748017e8017b2
gpalab <gpalabgeorgia@gmail.com>
2025-01-06 14:23:04 +0100

    Make Listing Page. Embed Listing Page HTML/Design

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class ProductsController extends Controller
 {
     public function listing($url) {
         $categoryCount = Category::where(['url'=>$url, 'status'=>1])->count();
         if($categoryCount>0) {
-            // echo "Category exists"; die;
-            $categoryDetails = Category::categoryDetails($url);
-            echo "<pre>"; print_r($categoryDetails); die;
+            $categoryDetails = Category::catDetails($url);
+            $categoryProducts = Product::whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
+            return view('front.products.listing')->with(compact('categoryDetails', 'categoryProducts'));
         } else {
             abort(404);
         }
